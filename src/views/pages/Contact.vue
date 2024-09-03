28c794661d27556a534de0260c2b7deafd2cf15d
abhishekmaha23 <abhishek.mahadevan23@gmail.com>
2024-09-03 02:14:02 +0200

    Change marker location

--- src/views/pages/Contact.vue
+++ src/views/pages/Contact.vue
@@ -42,8 +42,19 @@ onMounted(() => {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    // Add Marker
-    L.marker([51.4416, 5.4697]).addTo(map)
+    // Define custom icon using CDN resources
+    const customIcon = L.icon({
+        iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+        iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+
+    // Add Marker with custom icon
+    L.marker([51.4416, 5.4697], { icon: customIcon }).addTo(map)
         .bindPopup('Quintessence Research B.V.')
         .openPopup();
 });
